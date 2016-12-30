33565435fb78ce10669fef7fda968d9a3cc4930b
henry <944189562@qq.com>
2016-12-30 16:39:48 +0800

    route

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-<<<<<<< HEAD
-var app=angular.module("app",['dir','ngRoute','home']);
-=======
-var app=angular.module("app",["ctrl"]);
-console.log(app)
->>>>>>> 67a0ee0a3ea0a73b92d52377d128c2f0ade03c76
+var app = angular.module("app",['ngRoute']);
