c4070897efabf7ab1ef0fa6574ff2bcc4f9283c0
henry <944189562@qq.com>
2016-12-30 14:51:58 +0800

    fasd

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
 var app=angular.module("app",['dir','ngRoute','home']);
-var arr=[
-    {
-        url:'pages/register/images/pg.png',
-        text:'你好'
-    },
-    {
-        url:'pages/register/images/td.png',
-        text:'大家好'
-    },
-]
