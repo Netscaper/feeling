1def22400c16d02a4820da4474d65d14cad4d9a3
henry <944189562@qq.com>
2017-01-03 18:50:06 +0800

    demglu

--- route.js
+++
@@ -1,6 +0,0 @@
-app.config(['$routeProvider', function($routeProvider){
-                $routeProvider
-                .when('/',{templateUrl:'pages/search/search.html'})
-                .when('/login',{templateUrl:'pages/login/login1.html'})
-                .when('/register',{templateUrl:'pages/register/register.html'})
-            }])
