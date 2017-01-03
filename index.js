bcbea6cd22713ae6d93683189768792f40765a2a
leihongyan <1527220895@qq.com>
2017-01-03 16:38:46 +0800

    lhy

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
-var app = angular.module("app",['ngRoute','search']);
-=======
-
-var app = angular.module("app",['ngRoute','ctrl']);
->>>>>>> bec55737cbebad7da9a04351ed747b56f2427f34
+var app = angular.module("app",['ngRoute','search','ctrl']);
 
