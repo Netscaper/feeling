749d618ac3c55e61109072c29b7f88ec285fd568
henry <944189562@qq.com>
2017-01-04 13:35:00 +0800

    f

--- route.js
+++ route.js
@@ -2,10 +2,7 @@ app.config(['$routeProvider', function($routeProvider){
     $routeProvider
     .when('/',{templateUrl:'pages/home/home.html'})
     .when('/login',{templateUrl:'pages/login/login.html'})
-<<<<<<< HEAD
     .when('/login1',{templateUrl:'pages/login/login1.html'})
-=======
->>>>>>> 04e3ab99459bed1416b4fdbfaad58b96cec7730f
     .when('/chat',{templateUrl:'pages/chat/chat.html'})
     .when('/order',{templateUrl:'pages/pay/order.html'})
     .when('/albo',{templateUrl:'pages/pay/albo.html'})
