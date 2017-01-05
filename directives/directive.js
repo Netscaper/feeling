6c3dd01c0ed909526a90781e193391ddeac5c5d0
huyongbiao123 <954963927@qq.com>
2017-01-05 08:57:34 +0800

    'hyh'

--- directives/directive.js
+++ directives/directive.js
@@ -22,23 +22,11 @@ dir.directive("lxLan",function(){
         }
     }
 })
-<<<<<<< HEAD
 dir.directive("lhyssjg",function(){
     return {
         restrict:'ECMA',
         // replace:true,
         templateUrl:"../tpl/lhyssjg/lhyssjg.html",
         scope:true
-    }    
-=======
-dir.directive('order',function(){
-    return {
-        restrict : "ECMA",
-        templateUrl :"../pages/pay/tpl1.html",
-        replace:true,
-        scope:{
-            data:"=order"
-        }
-    };
->>>>>>> 3a6567aaf6cf959067bcca2d130d1004d43059b1
-})
+    }   
+}) 
--- route.js
+++ route.js
@@ -1,6 +1,7 @@
 app.config(['$routeProvider', function($routeProvider){
     $routeProvider
     .when('/',{templateUrl:'pages/home/home.html'})
+    .when('/flash',{templateUrl:'pages/flash/flash.html'})
     .when('/login',{templateUrl:'pages/login/login.html'})
     .when('/chat',{templateUrl:'pages/chat/chat.html'})
     .when('/order',{templateUrl:'pages/pay/order.html'})
