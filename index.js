7a82adf047c162d16c1f55c16889c2ff4c5f5c18
henry <944189562@qq.com>
2017-01-05 10:23:12 +0800

    zyp

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
-<<<<<<< HEAD
-var app = angular.module("app",['ngRoute','look','search','home','dir','ctrl','login','life']);
-=======
-<<<<<<< HEAD
-
-=======
 var app = angular.module("app",['ngRoute','search','home','dir','ctrl','login','order','life','music']);
->>>>>>> 382af616c0e828a73d341965596c9a4169adf74b
->>>>>>> d89acb16d7eea7f54046c5fdea888440f1eed361
 
