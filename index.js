c2a1993de88e0cc42824e0bc9faf4841098facca
leihongyan <1527220895@qq.com>
2017-01-05 19:25:44 +0800

    lhy

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-var app = angular.module("app",['ngRoute','search','home','dir','ctrl','login','order','life','music','look','chat','swiper']);
+var app = angular.module("app",['ngRoute','search','home','dir','ctrl','login','order','life','music','look','chat','swiper','wo']);
